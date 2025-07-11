79c65ec1ff8b6dda52c8fc065a661a18a5047bb2
Weebpaisen <Joserenatolon@gmail.com>
2025-07-11 05:02:08 -0300

    Versão Final

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 'use strict'; //Ativa o modo estrito do JavaScript, otimizando o código e evitando erros que poderiam sobrecarregar o navegador, ativo por padrão no HTML5.
 
-var images = [ //Array com objetos que contêm id e caminho para as imagens
-    { 'id': '1', 'url': './img/fc.jpg'},
-    { 'id': '2', 'url': './img/sc.jpg'},
-    { 'id': '3', 'url': './img/3rd.jpg'},
-    { 'id': '4', 'url': './img/zero.jpg'},
-    { 'id': '5', 'url': './img/azure.jpg'},
-    { 'id': '6', 'url': './img/cs.jpg'},
-    { 'id': '7', 'url': './img/cs2.jpg'},
-    { 'id': '8', 'url': './img/cs3.jpg'},
-    { 'id': '9', 'url': './img/cs4.jpg'},
-    { 'id': '10', 'url': './img/reverie.jpg'},
-    { 'id': '11', 'url': './img/db.jpg'},
-    { 'id': '12', 'url': './img/db2.jpg'},
-    { 'id': '13', 'url': './img/horizon.jpg'},
-    { 'id': '14', 'url': './img/1st.jpg'},
+let images = [ //Array com objetos que contêm id e caminho para as imagens
+    { 'id': '1', 'url': './img/1st.jpg'},
+    { 'id': '2', 'url': './img/fc.jpg'},
+    { 'id': '3', 'url': './img/sc.jpg'},
+    { 'id': '4', 'url': './img/3rd.jpg'},
+    { 'id': '5', 'url': './img/zero.jpg'},
+    { 'id': '6', 'url': './img/azure.jpg'},
+    { 'id': '7', 'url': './img/cs.jpg'},
+    { 'id': '8', 'url': './img/cs2.jpg'},
+    { 'id': '9', 'url': './img/cs3.jpg'},
+    { 'id': '10', 'url': './img/cs4.jpg'},
+    { 'id': '11', 'url': './img/reverie.jpg'},
+    { 'id': '12', 'url': './img/db.jpg'},
+    { 'id': '13', 'url': './img/db2.jpg'},
+    { 'id': '14', 'url': './img/horizon.jpg'},
 ]
 
-var container = document.querySelector ('#container-items'); 
+let containerItems = document.querySelector ('#container-items'); 
 
-var loadImages = (images, container) => {
+
+
+let loadImages = (images, container) => {
     images.forEach (image => { //Pra cada imagem no array, executa uma função
        /*Adiciona uma div e a imagem ao container através da innerhtml */ container.innerHTML +=`
         <div class='item'> 
@@ -30,5 +32,19 @@ var loadImages = (images, container) => {
 
 }
 
+loadImages(images, containerItems);
 
-loadImages(images, container);  
+let items = document.querySelectorAll('.item'); 
+
+let previous = () => {
+    containerItems.appendChild(items[0]); //Adiciona o primeiro item ao final do container
+    items = document.querySelectorAll('.item'); //Atualiza a lista de itens
+}
+
+let next = () => {
+     const lastItem = items[items.length - 1];
+     containerItems.insertBefore( lastItem, items[0] );
+     items = document.querySelectorAll('.item');
+}
+document.querySelector('#next').addEventListener('click', previous); //Adiciona o evento de click no botão de anterior, indo para a imagem anterior
+document.querySelector('#previous').addEventListener('click', next);
